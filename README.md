7581087d461e3ee395ef269753e70a907be0acef
JanaIsCoding <jana.istrate@gmail.com>
2022-11-04 13:15:07 +0200

    Upload js learning files

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # js-lessons
+
+
+This repository is just a JavaScript learning enviroment. 
+I will be taking important notes through my learning path here, for future reference and better organisation of content.
+
+- Similar to CSS styles, external scripts file is DA WAY: <script src="javascript.js"></script>
+
+- Name things right!: https://javascript.info/variables#name-things-right 
+
+- In the first task, https://javascript.info/variables#working-with-variables 
+declaring "name" showed as "deprecated". 
+
+https://stackoverflow.com/questions/65379554/why-does-my-variable-show-its-deprecated
+
---
+++ javascript.js
@@ -0,0 +1,43 @@
+// let message = 'Learning is fun!'; // define the variable and assign the value
+
+// alert(message); // Learning is fun!
+
+// const COLOR_RED = "#F00";
+// const COLOR_GREEN = "#0F0";
+// const COLOR_BLUE = "#00F";
+// const COLOR_ORANGE = "#FF7F00";
+
+// ...when we need to pick a color
+// let color = COLOR_ORANGE;
+// alert(color); // #FF7F00 
+
+// Working with variables tasks: https://javascript.info/variables#working-with-variables
+
+// Task 1;
+let admin;
+let name = "John";
+admin = name;
+// alert(admin);
+
+// Task 2;
+let ourPlanetName = "Earth";
+let currentUserName = "John";
+
+// The Odin Project Assignment
+//1. 
+console.log(23 + 97)
+//2.
+console.log(1+2+3+4+5+6)
+//3.
+console.log((4 + 6 + 9) / 77)
+//4.
+let a = 10;
+console.log(a)
+console.log(9*a)
+let b = 7 * a;
+console.log(b)
+//5. 
+const max = 57;
+let actual = max - 13;
+let percentange = actual / max ;
+

611c9a872827fe2140d8531396a87800970b8a82
JanaIsCoding <jana.istrate@gmail.com>
2022-11-08 13:47:46 +0200

    Complete JS Fundamentals Assignments TOP

--- javascript.js
+++ javascript.js
@@ -1,43 +1,27 @@
-// let message = 'Learning is fun!'; // define the variable and assign the value
+//Fundamentals Part 3 - The Odin Project: https://www.theodinproject.com/lessons/foundations-fundamentals-part-3#assignment
 
-// alert(message); // Learning is fun!
+//function add7(x) {
+//    console.log( x + 7);
+//}
 
-// const COLOR_RED = "#F00";
-// const COLOR_GREEN = "#0F0";
-// const COLOR_BLUE = "#00F";
-// const COLOR_ORANGE = "#FF7F00";
+//function multiply(a,b) {
+//    console.log(a*b);
+//}
 
-// ...when we need to pick a color
-// let color = COLOR_ORANGE;
-// alert(color); // #FF7F00 
+function capitalize(string)
+{
+     let one = (string[0].toUpperCase());
+     two = string.slice(1, ); 
+    // let rest = (two.toLowerCase());
+    newText = one + two; //shows just first letter capitalized 
+    // newText= one + rest; //shows first letter capitalized, everything else lower case.
+    console.log(newText);
+}
+// example: capitalize("i am A STRING")
 
-// Working with variables tasks: https://javascript.info/variables#working-with-variables
-
-// Task 1;
-let admin;
-let name = "John";
-admin = name;
-// alert(admin);
-
-// Task 2;
-let ourPlanetName = "Earth";
-let currentUserName = "John";
-
-// The Odin Project Assignment
-//1. 
-console.log(23 + 97)
-//2.
-console.log(1+2+3+4+5+6)
-//3.
-console.log((4 + 6 + 9) / 77)
-//4.
-let a = 10;
-console.log(a)
-console.log(9*a)
-let b = 7 * a;
-console.log(b)
-//5. 
-const max = 57;
-let actual = max - 13;
-let percentange = actual / max ;
-
+function lastLetter(string)
+{
+   last =  string.charAt(string.length-1); 
+    console.log(last);
+}
+// example: lastLetter("the last letter")
